50ab001f98b5710d71556eb1cfbc63e2b58b1631
BogMil <milanbogdanovic11@hotmail.com>
2020-10-20 14:30:26 +0200

    route fix

--- routes/api.php
+++ routes/api.php
@@ -20,4 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 
 
-Route::post('/handleCallback', [ViberController::class, 'handleCallback']);
+Route::post('/handlecallback', [ViberController::class, 'handleCallback']);
